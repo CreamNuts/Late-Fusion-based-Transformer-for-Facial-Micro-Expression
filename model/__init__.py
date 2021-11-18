cb6c5dc85a62d3b986be66f7cf40282a44bdd04d
CreamNuts <hong4497@gmail.com>
2021-11-18 10:26:56 +0000

    Modify model

--- model/__init__.py
+++ model/__init__.py
@@ -3,6 +3,7 @@ Code reference: https://github.com/rwightman/pytorch-image-models
 """
 
 from .create_model import create_model
+from .fusion import *
 from .resnext import *
 from .swin_transformer import *
 from .timesformer import *
